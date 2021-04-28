b371be62f648b448181b3d3c37b35035710d7cb1
unsurecoder <62917058+unsurecoder@users.noreply.github.com>
2021-04-28 23:30:08 +0530

    Update 15 Days of Learning SQL.sql

--- Advanced Join/15 Days of Learning SQL.sql
+++ Advanced Join/15 Days of Learning SQL.sql
@@ -9,3 +9,52 @@ submission_date ,(
 from 
 (select distinct submission_date from submissions) s1
 group by submission_date
+
+
+
+/*    NEW SOLUTION(WORKED)   */ 
+
+SELECT inl.*,
+       name
+FROM
+  (SELECT t2.submission_date,
+          t1.ucn,
+          min(t2.hacker_id) hacker_id
+   FROM
+     (SELECT submission_date,
+             count(DISTINCT hacker_id) ucn
+      FROM
+        (SELECT s.*,
+                dense_rank() over(
+                                  ORDER BY submission_date) AS date_rank,
+                dense_rank() over(PARTITION BY hacker_id
+                                  ORDER BY submission_date) AS hacker_rank
+         FROM submissions s)
+      WHERE hacker_rank=date_rank
+      GROUP BY submission_date,
+               date_rank,
+               hacker_rank)t1,
+     (SELECT submission_date,
+             max(cn) mcn
+      FROM
+        (SELECT submission_date,
+                hacker_id,
+                count(*) cn
+         FROM submissions
+         GROUP BY submission_date,
+                  hacker_id)
+      GROUP BY submission_date)t3,
+     (SELECT submission_date,
+             hacker_id,
+             count(*) cn
+      FROM submissions
+      GROUP BY submission_date,
+               hacker_id)t2
+   WHERE t1.submission_date=t2.submission_date
+     AND t2.submission_date=t3.submission_date
+     AND t2.cn=t3.mcn
+   GROUP BY t2.submission_date,
+            t1.ucn) inl,
+     hackers h
+WHERE inl.hacker_id=h.hacker_id
+ORDER BY 1;
